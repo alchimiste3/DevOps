b783fc5fceb1c147066cafe854650484e132f259
Clément <sc407514@etu.unice.fr>
2016-02-22 16:25:09 +0100

    Ajout Mutator + en -

--- Mutator/src/main/java/Main.java
+++ Mutator/src/main/java/Main.java
@@ -1,5 +1,18 @@
+
+import processor.BinaryOperatorMutator;
+
 /**
  * Created by sualty on 12/02/16.
  */
 public class Main {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Aucune source passée en paramètre");
+        }
+        else {
+            System.out.println("Application de la mutation sur le fichier" + args[0]);
+            BinaryOperatorMutator mutationOperator = new BinaryOperatorMutator();
+        }
+        
+    }
 }
---
+++ Mutator/src/main/java/processor/BinaryOperatorMutator.java
@@ -0,0 +1,22 @@
+package processor;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.BinaryOperatorKind;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.declaration.CtElement;
+
+public class BinaryOperatorMutator extends AbstractProcessor<CtElement> {
+	@Override
+	public boolean isToBeProcessed(CtElement candidate) {
+		return candidate instanceof CtBinaryOperator;
+	}
+
+	@Override
+	public void process(CtElement candidate) {
+		if (!(candidate instanceof CtBinaryOperator)) {
+			return;
+		}
+		CtBinaryOperator op = (CtBinaryOperator)candidate;
+		op.setKind(BinaryOperatorKind.MINUS);
+	}
+}
