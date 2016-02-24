b34a260d7d479035c3c603e9cc02f3dcb182382c
Bénédicte Lagouge <benedicte.lagouge@etu.unice.fr>
2016-02-24 12:17:53 +0100

    ligne de build fonctionnelle.

---
+++ SourcesUnderTest/src/main/java/package1/Classe1.java
@@ -0,0 +1,12 @@
+package package1;
+
+/**
+ * Created by sualty on 24/02/16.
+ */
+public class Classe1 {
+
+    public Classe1() {
+        int i=0;
+        i=i+1;
+    }
+}
---
+++ SourcesUnderTest/src/main/java/package2/Classe2.java
@@ -0,0 +1,12 @@
+package package2;
+
+/**
+ * Created by sualty on 24/02/16.
+ */
+public class Classe2 {
+
+    public Classe2() {
+        int i=0;
+        i=i+1;
+    }
+}
