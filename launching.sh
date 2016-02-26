61f158697e81dcb86e33c0e716a7aec1095dd3c0
Bénédicte Lagouge <benedicte.lagouge@etu.unice.fr>
2016-02-26 09:50:21 +0100

    réunion des projets Analysor et Mutator

--- launching.sh
+++ launching.sh
@@ -12,10 +12,9 @@ cd ../SourcesUnderTest
 #launching tests with one processor
 for p in ${!paths[@]}  
 do
-	mvn clean test -Dp_chemin_1=processor."${paths[p]}"
-	cd ../Analysor
-	mvn clean install
-	mvn exec:java -Dexec.mainClass="main.Main" -Dnom_fichier="${paths[p]}"
+	mvn -q clean test -Dp_chemin_1=processor."${paths[p]}"
+	cd ../Mutator
+	mvn -q exec:java -Dexec.mainClass="main.Main" -Dnom_fichier="${paths[p]}"
 done
 
 #launching tests with two processors
@@ -23,29 +22,8 @@ for p in ${!paths[@]}
 do
 	for pp in ${!paths[@]}  
 	do
-		mvn clean test -Dp_chemin_1=processor."${paths[p]}" -Dp_chemin_2=processor."${paths[pp]}"
-		cd ../Analysor
-		mvn clean install
-		mvn exec:java -Dexec.mainClass="main.Main" -Dnom_fichier="${paths[p]}""${paths[pp]}"
+		mvn -q clean test -Dp_chemin_1=processor."${paths[p]}" -Dp_chemin_2=processor."${paths[pp]}"
+		cd ../Mutator
+		mvn -q exec:java -Dexec.mainClass="main.Main" -Dnom_fichier="${paths[p]}""${paths[pp]}"
 		done
 done
-
-#launching tests with three processors
-mvn clean test -Dp_chemin_1=processor.GELE -Dp_chemin_2=processor.PlusMinus -Dp_chemin_3=processor.IncDec
-cd ../Analysor
-mvn clean install
-mvn exec:java -Dexec.mainClass="main.Main" -Dnom_fichier=GELEPlusMinusIncDec
-
-#for p in ${!paths[@]}  
-#do
-#	for pp in ${!paths[@]}  
-#	do
-#		for ppp in ${!paths[@]}  
-#		do
-#					done
-#	done
-#done
-
-
-
-
