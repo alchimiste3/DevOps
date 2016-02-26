359ef99d0ca79018dc08ed714fbad4108e6cec5e
Bénédicte Lagouge <benedicte.lagouge@etu.unice.fr>
2016-02-26 08:16:13 +0100

    ajout css viré hier, pardon

--- launching.sh
+++ launching.sh
@@ -31,19 +31,20 @@ do
 done
 
 #launching tests with three processors
-for p in ${!paths[@]}  
-do
-	for pp in ${!paths[@]}  
-	do
-		for ppp in ${!paths[@]}  
-		do
-			mvn clean test -Dp_chemin_1=processor."${paths[p]}" -Dp_chemin_2=processor."${paths[pp]}" -Dp_chemin_3=processor."${paths[ppp]}"
-			cd ../Analysor
-			mvn clean install
-			mvn exec:java -Dexec.mainClass="main.Main" -Dnom_fichier="${paths[p]}""${paths[pp]}""${paths[ppp]}"
-		done
-	done
-done
+mvn clean test -Dp_chemin_1=processor.GELE -Dp_chemin_2=processor.PlusMinus -Dp_chemin_3=processor.IncDec
+cd ../Analysor
+mvn clean install
+mvn exec:java -Dexec.mainClass="main.Main" -Dnom_fichier=GELEPlusMinusIncDec
+
+#for p in ${!paths[@]}  
+#do
+#	for pp in ${!paths[@]}  
+#	do
+#		for ppp in ${!paths[@]}  
+#		do
+#					done
+#	done
+#done
 
 
 
