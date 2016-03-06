b20f98b9cd25e498718beb488004c092081e1dc8
Bénédicte Lagouge <benedicte.lagouge@etu.unice.fr>
2016-03-06 15:30:55 +0100

    modification conf.xml et processeur GELE

--- Mutator/src/main/java/processor/GELE.java
+++ Mutator/src/main/java/processor/GELE.java
@@ -3,14 +3,30 @@ package processor;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 
 
 public class GELE extends AbstractProcessor<CtElement> {
 
+    private String nameOfClass="Classe1";
+    private int nb_of_applications=3;
+
     @Override
     public boolean isToBeProcessed(CtElement candidate) {
-        return candidate instanceof CtBinaryOperator;
+        int nb_found=0;
+        if(nb_found<nb_of_applications) {
+            if(candidate instanceof CtBinaryOperator) {
+                if(candidate.getParent(CtClass.class).getSimpleName().equals(nameOfClass)) {
+                    if(((CtBinaryOperator)candidate).getKind()==BinaryOperatorKind.LE
+                        ||((CtBinaryOperator)candidate).getKind()==BinaryOperatorKind.GE) {
+                        nb_found++;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     @Override
