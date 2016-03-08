b09371e01903c178414d10baf35b044e39e5e40d
Bénédicte Lagouge <benedicte.lagouge@etu.unice.fr>
2016-03-08 15:07:12 +0100

    les processeurs prennent en compte la configuration des classes à tester indiquée dans le fichier conf.xml

--- Mutator/src/main/java/processor/AbstractProjectProcessor.java
+++ Mutator/src/main/java/processor/AbstractProjectProcessor.java
@@ -1,9 +1,14 @@
 package processor;
 
+import analyseur.analyse.Mutant;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,14 +66,48 @@ abstract class AbstractProjectProcessor extends spoon.processing.AbstractProcess
             }
         return false;
     }
+
     /**
      * récupère la liste des classes où appliquer le processeur.Si cette liste est vide, on l'applique dans toutes les classes
      * @return la liste des classes où appliquer le processeur
      */
     public void setClasses() {
-        List<String> list = new ArrayList<String>();
+        this.classes=new ArrayList<String>();
+
+        String path = new java.io.File("").getAbsolutePath();
+        path=path.substring(0,path.lastIndexOf('/'));
+        path=path+"/conf.xml";
+        System.out.println(path);
+
+        SAXBuilder saxBuilder = new SAXBuilder();
+        File fileConf = new File(path);
+
+        try {
+            Document documentConf = saxBuilder.build(fileConf);
+            Element rootNode = documentConf.getRootElement();
+
+            Element processors = rootNode.getChild("processors");
+
+            List<Element> configs = processors.getChildren("processor");
+            Element config=null;
+            for(Element c : configs) {
+                if(c.getChildText("nom").equals(this.nom))
+                    config = c;
+            }
+
+            List<Element> classes = config.getChildren("classe");
+            for(Element e : classes) {
+                this.classes.add(e.getChildText("nom"));
+                System.out.println("\n\n\n"+e.getChildText("nom")+"\n\n\n");
+            }
+        } catch (Exception e) {
+            if(e instanceof NullPointerException) {
+                System.out.println("Pas de configuration prévue.Application à toutes les classes");
+                this.classes=new ArrayList<String>();
+              //  e.printStackTrace();
+            }
+        }
         //TODO:récupérer info du xml si existante . On utilise la variable "nom" pour savoir où chercher
-        this.classes=list;
     }
 
     /**
