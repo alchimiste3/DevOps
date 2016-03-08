a16708123098475082c9268958b4ed3242e52288
Clément <sc407514@etu.unice.fr>
2016-03-08 19:43:27 +0100

    Ajout test DoubleMutator

--- SourcesUnderTest/src/main/java/package1/Classe1.java
+++ SourcesUnderTest/src/main/java/package1/Classe1.java
@@ -4,11 +4,17 @@ public class Classe1 {
 
     private int j;
     private int k;
+    private double doubleVar;
     public Classe1() {
         j=0;k=0;
         j=j+1;
         if(k<=j)
             k++;
+
+        double a = 5;
+        double b = 2;
+        double c = a / b;
+        doubleVar = c;
     }
 
 
@@ -17,5 +23,9 @@ public class Classe1 {
     }
     public int getK() {
         return k;
+    }
+
+    public double getDoubleVar() {
+        return doubleVar;
     }
 }
--- SourcesUnderTest/src/test/java/package1/Classe1Test.java
+++ SourcesUnderTest/src/test/java/package1/Classe1Test.java
@@ -21,4 +21,10 @@ public class Classe1Test {
         Classe1 c = new Classe1();
         Assert.assertEquals(1,c.getK());
     }
+
+    @Test
+    public void testDouble() {
+        Classe1 c = new Classe1();
+        Assert.assertEquals(2.5,c.getDoubleVar(), 0.01);
+    }
 }
